40e29d7a1513f66b897d16b9d712c63aba7bdec4
fbenfraj <farouk.ben-fraj@sogeti.com>
2020-05-18 14:41:26 +0200

    Error message if server is unreachable

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../logo.svg";
 
 const Login = () => {
+  const [serverIsOnline, setServerIsOnline] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const checkServer = async () => {
+      const response = await fetch("http://localhost:8888/ping");
+      if (response.status === 200) {
+        setServerIsOnline(true);
+      }
+    };
+    checkServer();
+  }, []);
+
+  const login = () => {
+    if (serverIsOnline) {
+      window.location.href = "http://localhost:8888/login";
+    } else {
+      setErrorMessage("Couldn't reach the server. Please try again later.");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <main className="login">
           <h1>Welcome to Tinyspot</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>
-            Please connect to your Spotify account thanks to the button below.
-          </h2>
-          <a href="http://localhost:8888/login">
-            <button className="whiteButton">Login with Spotify</button>
-          </a>
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+            style={{ marginBottom: "20px" }}
+          />
+          <br />
+          <button onClick={login} className="whiteButton">
+            Login with Spotify
+          </button>
+          <br />
+          <p style={{ marginTop: 0, color: "red", fontSize: "15px" }}>
+            {errorMessage}
+          </p>
         </main>
         <footer id="footer" style={{ position: "fixed", bottom: 0 }}>
           <p>
